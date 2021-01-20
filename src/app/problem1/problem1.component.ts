743c02e7254c8756f392ac20cf28f3c50a99ef46
Matheus <matheuscostados.santos18@gmail.com>
2021-01-20 16:57:30 -0300

    problem 1 solved

--- src/app/problem1/problem1.component.ts
+++ src/app/problem1/problem1.component.ts
@@ -8,15 +8,35 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./problem1.component.scss']
 })
 export class Problem1Component implements OnInit{
-  textArea: string | undefined;
+  sorted_numbers: number[] | undefined;
+  arr_size: number | undefined;
 
   ngOnInit(): void {
-    this.textArea = '';
+    this.arr_size = 0;
+    this.sorted_numbers = [0];
   }
 
-  public extractNumbers(text: string): void{
+  public sortValues(text: string, arr_max_size: string): void{
+    let arr = this.extractNumbers(text, arr_max_size);
+    arr = this.removeDoubleValues(arr);
+    this.arr_size = arr.length;
+    this.sorted_numbers = arr;
+  }
+  
+  public extractNumbers(text: string, arr_size:string): number[]{
     const extractedNumbers = text.match(/-?\d+/g)!.map(Number);
-    console.log(extractedNumbers);
+    return extractedNumbers.slice(0, Number(arr_size));
+  }
+
+  public removeDoubleValues(arr: number[]): number[] {
+    const uniqueArr = arr
+      .filter((item, pos) => {
+        return arr.indexOf(item) == pos;
+      }).sort((a,b) => {
+        return a-b 
+      });
+
+    return uniqueArr;
   }
 
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
@@ -24,13 +44,13 @@ export class Problem1Component implements OnInit{
       if (matches) {
         return [
           { title: 'Problema', cols: 1, rows: 1 },
-          { title: 'Entrada', cols: 1, rows: 1 },
+          { title: 'Exemplo', cols: 1, rows: 1 },
         ]; 
       }
 
       return [
         { title: 'Problema', cols: 1, rows: 1 },
-        { title: 'Entrada', cols: 1, rows: 1 },
+        { title: 'Exemplo', cols: 1, rows: 1 },
       ];
     })
   );
